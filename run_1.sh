2975155fc42496daa2950237eaa1e2abceb7c1c2
pgs7179 <pgsnovel@naver.com>
2017-07-22 18:15:47 +0900

    init of gem5

---
+++ run_1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+time build/ARM/gem5.fast -d m5out_4cores4apps1 configs/example/fs.py --num-cpus=4 --caches --l2cache --machine-type=VExpress_EMM --disk-image=/home/hadi/apps/ece511/gem5/disks/homework3.img --kernel=/home/hadi/apps/ece511/gem5/binaries/vmlinux.aarch32.ll_20131205.0-gem5 --dtb-filename=/home/hadi/apps/ece511/gem5/binaries/vexpress.aarch32.ll_20131205.0-gem5.4cpu.dtb --script=/home/hadi/apps/ece511/gem5/4cores4apps1.rcS --cpu-type=atomic --mem-size=2048MB --mem-type=DDR3_1600_x64
